239620de46abc9c133b0cedf53222195e667437c
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2025-05-19 02:21:08 -0300

    fixed 4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,18 @@
 
-<x-header/>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Stock de Dina</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Sacramento&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+</head>
 <body class="imagenLogin">
 <div class="container-fluid p-4 color-nav rounded-5 shadow-lg position-absolute top-50 start-50 translate-middle" style="max-width: 300px;">
     <h2 class="mb-2 text-center text-light text-abril">Iniciar sesión</h2>
@@ -26,4 +39,4 @@
     </div>
 </div>
 </body>
-
+</html>
