6523f8e7ac72b63664bcc90831c5e75c47e8423c
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2025-05-19 02:46:45 -0300

    fixed 7

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Sacramento&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ secure_asset)'css/style.css') }}">
-    <link rel="icon" href="{{ secure_asset)'favicon.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+    <link rel="icon" href="{{ secure_asset('favicon.ico') }}" type="image/x-icon">
   </head>
 <body>
   <div id="app" class="d-flex flex-column min-vh-100">
